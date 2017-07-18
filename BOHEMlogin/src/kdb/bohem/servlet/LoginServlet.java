31f34249eccef655684bc23cbbb26185f68d81f2
jooyon <jooyon@jooyon-PC>
2017-07-18 14:22:01 +0900

    LoginServlet 추가 및 js 수정

---
+++ BOHEMlogin/src/kdb/bohem/servlet/LoginServlet.java
@@ -0,0 +1,23 @@
+package kdb.bohem.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		 
+		String id = request.getParameter("ID");
+		String pw =  request.getParameter("PW");
+	}
+
+}
